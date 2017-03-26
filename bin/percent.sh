3588e50db73d208e611bfe9321772ab769eb111b
James Jones <james@jones77.com>
2017-03-26 10:08:17 +0000

    Fixes refactor bugs
    Also, fixes a bug in pre/past add path.

--- bin/percent.sh
+++ bin/percent.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 # Percentage printer.  eg percent.sh 50 100 => "50%"
 
-. `type -p jjlib.sh` && usage_exit "$#" 2 "`basename $0` part total" "$@"
+usage_exit() {
+    # ARGUMENTS:
+    #   argc, argc_check, usage
+    # EXAMPLE:
+    #   usage_exit "$#" 2 "`basename $0` part total" "$@"
+    argc="$1"
+    argc_check="$2"
+    usage="$3"
+    if (( argc != argc_check ))
+    then
+        echo "Usage:    $usage"
+        exit 1
+    fi
+}
+
+usage_exit "$#" 2 "`basename $0` part total" "$@"
 
 num=$(echo "scale=2; ($1 / $2) * 100" | bc | sed 's/[.].*//')
 
