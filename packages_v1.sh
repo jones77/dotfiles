eed7857e812eb93f752343bcb810f490e5f1f147
James Jones <james@jones77.com>
2017-07-05 14:43:49 -0400

    Saving copy of previous package.sh system.

---
+++ packages_v1.sh
@@ -0,0 +1,225 @@
+#!/usr/bin/env bash
+#
+# configure -h for help, quick and dirty developer install
+#
+# Note: Installing Linuxbrew on RHEL needs the following workaround:
+# https://github.com/Linuxbrew/brew/issues/340#issuecomment-294900797
+cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd  # cd $script_dir
+source dotfiles/shelllib.sh
+declare -r basename=$(basename $0)
+declare -r indent=$(echo $basename | sed 's/./~/g')
+# -----------------------------------------------------------------------------
+#
+# BEGIN functions
+#
+declare -r args="ahlpdxy"
+function usage {
+    echo "Usage:    $basename -$args"
+    cat <<EOF
+a) all
+h) help
+l) install Linuxbrew packages: $(list_packages l)
+p) install default packages: $(list_packages p)
+d) install developer packages: $(list_packages d)
+x) install desktop packages: $(list_packages x)
+y) install pip packages: $(list_packages y)
+EOF
+    exit 1
+}
+function list_packages {
+    echo $( echo $(cat _packages/$1) )
+}
+declare -r package_files=$(ls _packages/*)  # List the packages lists
+distropkgs=""  # Note: First package added adds a leading space
+while getopts ":$args" opt
+do
+    case $opt in
+	a)  # All packages
+        for file in $package_files
+        do
+            f=$(basename $file)
+            # Linuxbrew and Python packages are different
+            [[ "$f" != "l" ]] && [[ "$f" != "y" ]] \
+                && distropkgs="$distropkgs $(list_packages $(basename $file))"
+        done
+        linuxbrew_pkgs="$(list_packages l)"
+        pip_pkgs="$(list_packages y)"
+    ;;
+        h) usage
+    ;;
+        l) linuxbrew_pkgs="$(list_packages l)"
+    ;;
+        y) pip_pkgs="$pip_pkgs $(list_packages y)"
+    ;;
+	?)  # A specific package, quit if it doesn't exist
+        if [[ -f "_packages/$opt" ]]
+        then
+            l=$(list_packages $opt)
+            distropkgs="$distropkgs $l" \
+                && echo $(ce Green $basename): Adding: $(ce green $l)
+
+            if [[ "$opt" == "d" ]]
+            then
+                pip_pkgs="$pip_pkgs virtualenvwrapper"
+            fi
+        else
+            echo "$basename: Quitting, Unknown opt: $opt"
+            usage
+        fi
+    ;;
+    esac
+done
+shift $((OPTIND-1))
+#
+[[ -z "$distropkgs" ]] && distropkgs=$(list_packages p)  # Default
+#
+#
+# END args
+#
+# -----------------------------------------------------------------------------
+#
+# BEGIN distro package configuration
+#
+if $(hash lsb_release 2>/dev/null)
+then
+    # Ubuntu, RedHat
+    distro=$(hash lsb_release && lsb_release -i | cut -f2)
+elif $(hash pacman 2>/dev/null)
+then
+    distro="ArchLinux"
+elif $(grep -i centos /etc/os-release 2>/dev/null 1>&2)
+then
+    distro="Centos"
+elif $(uname -a | grep FreeBSD >/dev/null 2>&1)
+then
+    distro="FreeBSD"
+else
+    ce Red "$(ce Green $basename): Unknown distro: uname -a=$(uname -a)"
+    exit 1
+fi
+
+ce White "$(ce Green $basename): Installing $(ce Yellow $distro) Packages:"
+ce Yellow "${indent}  $(ce Green ${distropkgs})"
+
+case $distro \
+in
+
+Centos|RedHatEnterpriseServer)
+# Linuxbrew workarounds for Centos 7.3
+# Building from source might be overkill
+export HOMEBREW_BUILD_FROM_SOURCE=1
+# vim wouldn't compile without brew's perl
+# vim wouldn't run without brew's python
+PRE_LINUX_BREW_INSTALL="echo yes \
+    | /home/linuxbrew/.linuxbrew/bin/brew install gcc perl python"
+
+# FIXME: Make installing 'Development Tools' dependent on -d
+sudo yum groupinstall -y 'Development Tools'
+# FIXME: Make 'X Window System' install dependent on -x
+sudo yum groupinstall -y 'X Window System'
+# FIXME: Do we need epel-release if we rely on Linuxbrew?
+# sudo yum install -y epel-release
+# FIXME: Aren't these redundant?
+sudo yum install -y irb python-devel python-setuptools
+
+declare -r install_cmd="sudo yum install -y $distropkgs"
+;;
+
+Debian)
+sudo apt-get update -y
+# FIXME: Create per OS install scripts.
+declare -r install_cmd="sudo apt-get install -y $distropkgs \
+    python-pip x11-xserver-utils"
+
+# Firefox
+# debmozlist="/etc/apt/sources.list.d/debian-mozilla.list"
+# debmozkeyring="pkg-mozzila-archive-keyring_1.1_all.deb"
+# if [[ ! -f "$debmozlist" ]]
+# then
+#     # https://www.google.com/search?q=NO_PUBKEY+85A3D26506C4AE2A
+#     # http://www.hangelot.eu/?p=209&lang=en
+#     sudo apt-get install debian-keyring
+#     gpg --keyserver keys.gnupg.net --recv-key 06C4AE2A
+#     gpg -a --export 06C4AE2A | sudo apt-key add -
+#     # https://medium.com/@mos3abof/how-to-install-firefox-on-debian-jessie-90fa135e9e9
+#     sudo touch "$debmozlist"
+#     echo 'deb http://mozilla.debian.net/ jessie-backports firefox-release' \
+#         | sudo tee "$debmozlist"
+#     wget "mozilla.debian.net/$debmozkeyring"
+#     sudo dpkg -i            "$debmozkeyring"
+#     sudo apt-get update -y
+#     sudo apt-get install -y -t jessie-backports firefox
+#     sudo rm "$debmozkeyring"
+# fi
+;;
+
+ArchLinux)
+
+# Note: --noconfirm: https://unix.stackexchange.com/a/52278 
+# Note: ArchLinux's decision to make /usr/bin/python
+# be Python3 is causing issues installing virtualenv.
+# https://wiki.archlinux.org/index.php/python
+# FIXME: Install virtualenvwrpper, python developer packages
+declare -r install_cmd="sudo pacman -Syu --noconfirm $(
+    echo $distropkgs $(echo '
+'))"
+;;
+Ubuntu)
+# FIXME: Ubuntu specific instead of defaulting to Ubuntu
+# elif [[ "$distro" = "???ubuntu" ]]
+declare -r install_cmd="sudo apt-get install -y echo $distropkgs python-dev"
+;;
+FreeBSD)
+declare -r install_cmd="sudo pkg"
+sudo pkg install git tmux vim \
+    xorg hs-xmonad hs-xmonad-contrib hs-xmobar \
+    :
+;;
+*)
+ce Red "$(ce Green $basename): Unknown distro: uname -a=$(uname -a)"
+exit 1
+;;
+esac
+$install_cmd
+#
+# END distro configuration
+#
+# -----------------------------------------------------------------------------
+#
+# BEGIN Linuxbrew configuration
+#
+if [[ -n "$linuxbrew_pkgs" ]]
+then
+    brewcmd="/home/linuxbrew/.linuxbrew/bin/brew"
+    [[ -f "$brewcmd" ]] || ( echo yes | ruby -e "$(curl -fsSL \
+        'https://raw.githubusercontent.com/Linuxbrew/install/master/install')"
+    )
+    eval "$PRE_LINUX_BREW_INSTALL"
+    echo yes | $brewcmd install curl git
+    for lb_package in $linuxbrew_pkgs
+    do
+        echo yes | $brewcmd install $linuxbrew_pkgs
+    done
+
+    # Haskell stack specific
+    # www.stephendiehl.com/posts/vim_2016.html
+    # echo yes | $brewcmd install stack
+    # stack setup
+    # stack install hlint ghc-mod
+fi
+#
+# END Linuxbrew configuration
+#
+# -----------------------------------------------------------------------------
+#
+# BEGIN pip configuration
+#
+# FIXME: Should probably force linuxbrew verison of Python to be consistent.
+sudo pip install --upgrade pip
+for p in $pip_pkgs
+do
+    sudo pip install $p
+done
+#
+# END pip configuration
+#
