1286bb61e1befec1ebe75d49ef6637e0818686e4
James Jones <jjones18@bloomberg.net>
2019-03-29 09:59:54 -0400

    More tmux right-side colour fixes

--- home/bin/tmux-cpu-info.py
+++ home/bin/tmux-cpu-info.py
@@ -6,14 +6,24 @@ import psutil
 
 
 DEBUG = False
-RED = '#[fg=red,bg=black]'
-MAGENTA = '#[fg=magenta,bg=black]'
-EMERGENCY = '#[fg=cyan,bg=black]'
-GREEN = '#[fg=colour46,bg=black]'
-YELLOW = '#[fg=yellow,bg=black]'
-GREY = '#[fg=colour243,bg=black]'
-INTRO = '#[fg=colour243,bg=black]#'
+RED = '#[fg=red,bg=black,noreverse]'
+RED_REVERSE = '#[fg=red,bg=black,reverse]'
+MAGENTA = '#[fg=magenta,bg=black,noreverse]'
+EMERGENCY = '#[fg=cyan,bg=black,noreverse]'
+GREEN = '#[fg=colour46,bg=black,noreverse]'
+GREEN_REVERSE = '#[fg=colour46,bg=black,reverse]'
+YELLOW = '#[fg=yellow,bg=black,noreverse]'
+YELLOW_REVERSE = '#[fg=yellow,bg=black,reverse]'
+GREY = '#[fg=colour243,bg=black,noreverse]'
+INTRO = '#[fg=colour243,bg=black,noreverse]#'
 
+ALERT_FMT = '#[fg=white,bg=black,reverse]#{alert}'
+
+
+CPU_LOW = GREEN
+CPU_HIGH = YELLOW
+CPU_110 = RED
+CPU_CRAZY = EMERGENCY
 
 if __name__ == '__main__':
     cpu_list = sorted(
@@ -24,13 +34,13 @@ if __name__ == '__main__':
     fmts = []
     for cpu in cpu_list:
         if cpu < 50.0:
-            fmts.append(GREEN)
+            fmts.append(CPU_LOW)
         elif cpu >= 50.0 and cpu < 100.0:
-            fmts.append(YELLOW)
+            fmts.append(CPU_HIGH)
         elif cpu >= 100.0 and cpu < 110.0:
-            fmts.append('{}'.format(RED))
+            fmts.append('{}'.format(CPU_110))
         elif cpu >= 110.0:
-            fmts.append('{}'.format(EMERGENCY))
+            fmts.append('{}'.format(CPU_CRAZY))
     # print(fmts)
 
     # FIXME: Do fmts with deciles
@@ -55,17 +65,23 @@ if __name__ == '__main__':
         display += decile
 
     pct = vm.available / vm.total
-    pct_fmt = '{0:.0%}'.format(pct)
-    print('{dir_colour}{dir}'
-          '{ram_colour}{ram_gb}G{pct_colour}{pct_fmt}'
-          '{cpu_colour}{display_len}CPU{display}'.format(
-              dir_colour=GREEN,
-              dir=sys.argv[1][-30:],  # Truncate cwd starting from the back
-              intro=INTRO,
-              ram_colour=GREY,
-              ram_gb=int(round(vm.total/1024/1024/1024)),
-              pct_colour=GREEN,
-              pct_fmt=pct_fmt,
-              cpu_colour=GREY,
-              display_len=len(deciles),
-              display=display))
+    pct_str = '{0:.0}'.format(pct)
+    if pct_str.startswith('0.'):
+        pct_str = pct_str[2:]
+    else:
+        pct_str = '!' + pct_str + '!'
+
+    print(
+        '{dir_colour}{dir}'
+        '{ram_colour}{ram_gb}G{pct_colour}{pct_str}'
+        '{display}'
+        .format(
+            dir_colour=GREEN,
+            # TODO: Indicate path was truncated
+            dir=sys.argv[1][-30:],  # Truncate cwd starting from the back
+            intro=INTRO,
+            ram_colour=GREY,
+            ram_gb=int(round(vm.total/1024/1024/1024)),
+            pct_colour=GREEN_REVERSE,
+            pct_str=pct_str,
+            display=display))
