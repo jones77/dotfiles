2cbd58777be833c83ad6469bd21cc3d2ce7e973f
James Jones <james@jones77.com>
2020-01-21 22:30:13 -0500

    Reorder cpu/mem info for clarity

--- home/bin/tmux-cpu-info.py
+++ home/bin/tmux-cpu-info.py
@@ -25,10 +25,7 @@ CPU_110 = RED
 CPU_CRAZY = EMERGENCY
 
 if __name__ == '__main__':
-    cpu_list = sorted(
-        psutil.cpu_percent(interval=0.1, percpu=True),
-    )
-    # print(cpu_list)
+    cpu_list = sorted(psutil.cpu_percent(interval=0.1, percpu=True))
     vm = psutil.virtual_memory()
     fmts = []
     for cpu in cpu_list:
@@ -40,7 +37,6 @@ if __name__ == '__main__':
             fmts.append('{}'.format(CPU_110))
         elif cpu >= 110.0:
             fmts.append('{}'.format(CPU_CRAZY))
-    # print(fmts)
 
     # FIXME: Do fmts with deciles
 
@@ -63,33 +59,23 @@ if __name__ == '__main__':
         display += fmts[i]
         display += decile
 
-    pct = vm.available / vm.total
-    pct_str = '{0:.0}'.format(pct)
-    if pct_str.startswith('0.'):
-        pct_str = pct_str[2:]
+    memory_used = vm.available / vm.total
+    mem_digit = '{0:.0}'.format(memory_used)
+    if mem_digit.startswith('0.'):
+        mem_digit = mem_digit[2:]
     else:
-        pct_str = '!' + pct_str + '!'
+        mem_digit = '!' + mem_digit + '!'
 
     stat = os.statvfs('.')
-    # posix.statvfs_result(f_bsize=4096, f_frsize=4096, f_blocks=65533179,
-    # f_bfree=20767004, f_bavail=20767004, f_files=131071424,
-    # f_ffree=130390422, f_favail=130390422, f_flag=4096, f_namemax=255)
     du_used = int(round((1 - stat.f_bavail / stat.f_blocks) * 100))
 
+    cwd=sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    ram_gb=int(round(vm.total/1024/1024/1024))
+
     print(
-        '{cwd_colour}{cwd}'
-        '{du_colour}{du}'
-        '{ram_colour}{ram_gb}G{pct_colour}{pct_str}'
-        '{display}'
-        .format(
-            cwd_colour=GREEN,
-            # TODO: Indicate path was truncated
-            cwd=sys.argv[1][-30:],  # Truncate cwd starting from the back
-            intro=INTRO,
-            du_colour=GREEN_REVERSE,
-            du=du_used,
-            ram_colour=GREY,
-            ram_gb=int(round(vm.total/1024/1024/1024)),
-            pct_colour=GREEN_REVERSE,
-            pct_str=pct_str,
-            display=display))
+        f'{GREEN}{cwd[-30:]}'
+        f'{GREEN_REVERSE}{du_used}'
+        f'{GREY}{ram_gb}G'
+        f'{GREEN_REVERSE}{mem_digit}'
+        f'{display}'
+      )
