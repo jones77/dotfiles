079bc034fb5759580f8721e764f58e695883076c
James Jones <jjones18@bllomberg.net>
2020-02-14 18:45:17 +0000

    Useful dependencies

---
+++ work.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# TODO: Move to packages.sh
+
+# vim w3m plugin needs w3m
+sudo yum install w3m
+
+# tmux-cpu-info.py needs psutil
+sudo /opt/bb/bin/apt-get install python3.7-psutil
