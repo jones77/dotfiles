e399190abb8709714b076acbd11f95f534f5cec1
James Jones <james@jones77.com>
2020-02-15 06:52:26 -0500

    Add cpu visualization

--- home/bin/tmux-cpu-info.py
+++ home/bin/tmux-cpu-info.py
@@ -7,65 +7,52 @@ import psutil
 
 DEBUG = False
 
-BACKGROUND = ',bg=colour17'
 RED = f'#[fg=red]'
-RED_REVERSE = f'#[fg=red{BACKGROUND},reverse]'
-MAGENTA = f'#[fg=magenta]'
+RED_REVERSE = f'#[fg=red,reverse]'
+MAGENTA = f'#[fg=colour13]'
 PINK = f'#[fg=colour217,nobold]'
 CYAN = f'#[fg=colour75]'
-EMERGENCY = f'#[fg=cyan{BACKGROUND},noreverse]'
 GREEN = f'#[fg=colour36,nobold]'
 GREEN = f'#[fg=colour36,nobold]'
 GREEN_BOLD = f'#[fg=colour36,bold]'
-GREEN_REVERSE = f'#[fg=colour36{BACKGROUND},reverse]'
-YELLOW = f'#[fg=colour227]'
-YELLOW_REVERSE = f'#[fg=colour227{BACKGROUND},reverse]'
+GREEN_REVERSE = f'#[fg=colour36,reverse]'
+YELLOW = f'#[fg=colour227,noreverse]'
+YELLOW_REVERSE = f'#[fg=colour227,reverse]'
 GREY = f'#[fg=colour243]'
-GREY_REVERSE = f'#[fg=colour243{BACKGROUND},reverse]'
-# FIXME: What's intro?
-INTRO = f'#[fg=colour243,noreverse]#'
+GREY_REVERSE = f'#[fg=colour243,reverse]'
+EMERGENCY = f'#[fg=red,bold]'
+CWD_LEN = 30
 
-CPU_LOW = GREEN
-CPU_HIGH = MAGENTA
-CPU_110 = RED
-CPU_CRAZY = EMERGENCY
+
+def cpu_viz():
+    """cpu_viz returns a tmux visualization of cpu usage."""
+    cpu_usage = "_▁▂▃▄▅▆▇██"  # ascending cpu usage indicators
+    cpu_list = sorted(psutil.cpu_percent(interval=0.5, percpu=True))
+    cpu_string = PINK  # tmux "visualization"
+
+    for cpu in cpu_list:
+        cpu_usage_index = int(cpu // len(cpu_usage))
+        if cpu_usage_index >= len(cpu_usage):
+            # Coerce to last element, cpu_usage can be >= 100.0
+            cpu_usage_index = len(cpu_usage) - 1
+        if cpu_usage_index == len(cpu_usage) - 1:
+            cpu_string += EMERGENCY
+        elif cpu_usage_index > (len(cpu_usage) // 2):
+            cpu_string += PINK
+        else:
+            cpu_string += GREEN
+        cpu_string += cpu_usage[cpu_usage_index]
+
+    return cpu_string
 
 
 if __name__ == '__main__':
-    cpu_list = sorted(psutil.cpu_percent(interval=0.1, percpu=True))
+    cwd = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    if len(cwd) > CWD_LEN:
+        cwd = '…' + cwd[-CWD_LEN-1:]
+    # cwd = cwd.replace('/', f'{YELLOW}/{GREEN}') + YELLOW + '/'
+
     vm = psutil.virtual_memory()
-    fmts = []
-    for cpu in cpu_list:
-        if cpu < 50.0:
-            fmts.append(CPU_LOW)
-        elif cpu >= 50.0 and cpu < 100.0:
-            fmts.append(CPU_HIGH)
-        elif cpu >= 100.0 and cpu < 110.0:
-            fmts.append('{}'.format(CPU_110))
-        elif cpu >= 110.0:
-            fmts.append('{}'.format(CPU_CRAZY))
-
-    # FIXME: Do fmts with deciles
-
-    deciles = []  # string collection of cpu percentages
-    for cpu in cpu_list:
-        # TODO: Add option to use just "traffic lights" and no numbers
-        decile_cpu = str(int(cpu / 10))
-        if len(decile_cpu) == 1:
-            deciles.append('{}'.format(decile_cpu))
-        elif decile_cpu == '10':
-            deciles.append('♨')
-        elif decile_cpu == '11':
-            deciles.append('!')
-        else:
-            deciles.append('<{}>'.format(decile_cpu))
-
-    display = ''
-    counter = 0
-    for i, decile in enumerate(deciles):
-        display += fmts[i]
-        display += decile
-
     memory_used = 1 - (vm.available / vm.total)
     mem_digit = '{0:.0}'.format(memory_used)
     if mem_digit.startswith('0.'):
@@ -76,13 +63,14 @@ if __name__ == '__main__':
     stat = os.statvfs('.')
     du_used = int(round((1 - stat.f_bavail / stat.f_blocks) * 100))
 
-    cwd=sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
     ram_gb=int(round(vm.total/1024/1024/1024))
 
+    cpu_string = cpu_viz()
+
     print(
-        f'{YELLOW}{cwd[-30:]}'
+        f'{YELLOW}{cwd}'
         f'{PINK}{du_used}'
         f'{GREEN}{ram_gb}'
-        f'{PINK}{mem_digit}'
-        f'{display}'
+        f'{YELLOW}{mem_digit}'
+        f'{cpu_string}{YELLOW}'
     )
--- home/bin/tmuxcolour.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sum=$(hostname | sum | cut -f1 -d' ')
-echo "colour$(expr $sum % 256)"
